5c7429d96e2b6ff8a724b13bc89fcf0dc82a4ca7
dhruvtailor <156967351+erdhruvtailor@users.noreply.github.com>
2024-08-14 05:52:55 -0400

    Merged Docker-CI-CD repository (#1)

---
+++ src/test/java/com/dt/learningtrial/dockercicd/DockerCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dt.learningtrial.dockercicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerCiCdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
